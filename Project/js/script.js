be87d0776fd657036ad74aa273ba3fa8cf975aeb
sohaib-essam <afifimohammedessam@gmail.com>
2022-06-29 00:53:35 +0200

    Finish Sidebar (Lessons [12 - 14])

--- Project/js/script.js
+++ Project/js/script.js
@@ -3,4 +3,9 @@ $(function () {
   $(".toggler").on("click", function () {
     $(".page-content, .sidebar").toggleClass("no-sidebar")
   });
+
+  $(".toggle-sub-links").on('click', function (e) {
+      $(this).find(".icon").toggleClass("fa-caret-down fa-caret-right");
+      $(this).next(".sub-links").slideToggle();
+  })
 });
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,1 @@
-$(function(){"use strict";$(".toggler").on("click",function(){$(".page-content, .sidebar").toggleClass("no-sidebar")})});
+$(function(){"use strict";$(".toggler").on("click",function(){$(".page-content, .sidebar").toggleClass("no-sidebar")}),$(".toggle-sub-links").on("click",function(n){$(this).find(".icon").toggleClass("fa-caret-down fa-caret-right"),$(this).next(".sub-links").slideToggle()})});
