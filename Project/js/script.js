192c3215108d80e195723d0d40e2f665d74a6844
sohaib-essam <afifimohammedessam@gmail.com>
2022-06-29 20:16:18 +0200

    Create Full Screen Icon (Lesson 18)

--- Project/js/script.js
+++ Project/js/script.js
@@ -1,11 +1,50 @@
 $(function () {
+
   "use strict";
+
+  // Toggle [Sidebar Apperance] & [Content Width]
   $(".toggler").on("click", function () {
     $(".page-content, .sidebar").toggleClass("no-sidebar")
   });
 
+  //--------------------------------------------------------------------------------------
+
+  // Toggle Dropdown
   $(".toggle-sub-links").on('click', function (e) {
       $(this).find(".icon").toggleClass("fa-caret-down fa-caret-right");
       $(this).next(".sub-links").slideToggle();
   })
+
+  //--------------------------------------------------------------------------------------
+
+  // Toggle FullScreen View
+  $(".maxmize").on("click", function () {
+    $(this).toggleClass("full-screen");
+    if ($(this).hasClass("full-screen")) {
+      openFullscreen()
+    } else {
+      closeFullscreen()
+    }
+  });
+  let elem = document.documentElement;
+  /* View in fullscreen */
+  function openFullscreen() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+  }
+  /* Close fullscreen */
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+  }
 });
